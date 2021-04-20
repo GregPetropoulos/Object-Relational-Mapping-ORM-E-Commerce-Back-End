af93e3a7f32f60e61b9a4377d3454c1c560a20a0
GP <gregpetropoulos@yahoo.com>
2021-04-20 06:04:30 -0400

    Set up the model folders Category.js,Product.js,ProductTag.js, and Tag.js, additionally used the index.js file to import models and make aasociations, and lastly export

--- models/index.js
+++ models/index.js
@@ -1,16 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Categories have many Products
-
+// Categories have many Products???
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   Product,
