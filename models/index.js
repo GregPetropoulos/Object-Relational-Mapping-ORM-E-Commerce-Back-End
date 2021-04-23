1404645e763b6ab22f1589b5dc39ddce01dd6122
GP <gregpetropoulos@yahoo.com>
2021-04-23 05:14:22 -0400

    Seeded ecommerce_db  database (mysql workbench succesfully) with npm run seeds

--- models/index.js
+++ models/index.js
@@ -13,17 +13,17 @@ Product.belongsTo(Category, {
 // Categories have many Products???
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, {
+Product.belongsToMany(Tag, {
+  through: ProductTag,
   foreignKey: "product_id",
-  onDelete: "CASCADE",
+  
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
+Tag.belongsToMany(Product, {
+  through: ProductTag,
   foreignKey: "tag_id",
-  onDelete: "CASCADE",
 });
 
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,45 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
+try {
+    const categoryData = await Category.findByPk(req.params.id);
+// may need to use includes here
+    if (!categoryData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
